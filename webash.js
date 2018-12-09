f3f9290ee5f2e91829d915273112e9e13db4f1a0
Tyler Grunenwald <tylergrunenwald@gmail.com>
2018-12-08 22:57:14 -0600

    refactored internal file structure representation, more robust path parsing

--- webash.js
+++ webash.js
@@ -23,98 +23,135 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
-const HOME = "/home/guest"
-const dirs = new Object()
-dirs[HOME] = ["about", "resume"]
-dirs[HOME + "/projects"] = ["website"]
-dirs[HOME + "/projects/github"] = ["hello-world"]
+const HOME = "/var/www"
 
 var CWD = HOME // current working directory
 
-function ls(dir) {
-	if (!dir) {
-		dir = CWD
-	}
-
-	fileOp(dir, (fname, dname, findex) => {
-		let file = document.createElement('div')
-		file.textContent = dirs[dname][findex]
-		file.className = 'file'
-		term._output.appendChild(file)
-	}, (fname, dname, findex) => {
-		if (Object.keys(dirs).includes(fname)) {
-			let newListing = document.createElement('div')
-			files = dirs[fname]
-			for (x in files) {
-				let span = document.createElement('span')
-				span.textContent = files[x]
-				span.className = 'file'
-				newListing.appendChild(span)
+// dirs/links/files are prefixed with an underscore
+const _root = {
+	type: 0,
+	content: {
+		_var: {
+			type: 0, // 0 = dir, 1 = link, 2 = file
+			content: {
+				_www: {
+					type: 0,
+					content: {
+						_about: {
+							type: 2,
+							content: "This is the about page"
+						},
+						_github: {
+							type: 1,
+							content: "https://github.com/tdgrunenwald"
+						},
+						_blog: {
+							type: 0,
+							content: {
+								_hello_world: {
+									type: 2,
+									content: "Hello, World!"
+								},
+								_archived: {
+									type: 0,
+									content: {
+										_old_crap: {
+											type: 2,
+											content: "This is an old dusty file"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
-			subDirs = getSubDirs(fname)
-			for (x in subDirs) {
-				let span = document.createElement('span')
-				span.textContent = subDirs[x]
-				span.className = 'directory'
-				newListing.appendChild(span)
-			}
-			term._output.appendChild(newListing)
-		} else {
-			bashError("ls", fname, "No such file or directory")
 		}
-	})
+	}
 }
 
-function cd(dir) {
-	if (!dir) { 
-		CWD = HOME
+function parsePath(path) {
+	if (!path) {
+		path = CWD
 	} else {
-		fileOp(dir, (fname, dname, findex) => {
-			bashError("cd", fname, "Not a directory")
-		}, (fname, dname, findex) => {
-			if (Object.keys(dirs).includes(fname)) {
-				CWD = fname
-			} else {
-				bashError("cd", fname, "No such file or directory")
+		// don't distinguish - vs _ or upper vs lower case
+		path = path.replace("-", "_").toLowerCase()
+	}
+
+	let parray = path.split("/")
+	if (!parray[parray.length - 1]) parray.pop()
+	if (!parray[0]) {
+		parray.shift()
+	} else {
+		// convert relative paths to absolute
+		let cwd = CWD.split("/")
+		if (!cwd[0]) cwd.shift()
+
+		for (let i in parray) {
+			switch (parray[i]) {
+				case ".":
+					continue
+					break
+				case "..":
+					cwd.pop()
+					break
+				default:
+					cwd.push(parray[i])
 			}
-		})
+		}
+		parray = cwd
+		path = "/" + cwd.join("/")
 	}
+
+	try {
+		var dir = _root
+		for (let i in parray) {
+			dir = dir.content["_" + parray[i]]
+		}
+	} catch (e) {
+		term.print(e)
+		dir = null
+		path = CWD
+	}
+	
+	return [dir, path]
 }
 
-function cat(file) {
-	if (!file) {
-		// echo stdin to stdout
-		// TODO: doesn't work...
-		term.input("HI CAT", function (input) {
-			term.print(input)
-		})
-	} else {
-		fileOp(file, (fname, dname, findex) => {
-			let fcontent = document.createElement('div')
-			readLocalFile(file, (content) => {
-				// on success
-				fcontent.innerHtml = content
-				term._output.appendChild(fcontent)
-			}, () => {
-				// on fail
-				term.print("BIG FAIL")
-			})
-		}, (fname, dname, findex) => {
-			term.print("cat: " + fname + ": No such file or directory")
-		})
-	}
+function bashError(func, arg, error) {
+	term.print("-bash: " + func + ": " + arg + ": " + error)
+}
+
+function ps1() {
+	return "guest@tylergrunenwald.com " + CWD + " $ "
 }
 
 const functions = {
 	clear: (argv) => { term.clear() },
 	pwd: (argv) => { term.print(CWD) },
 	echo: (argv) => { 
-		let line = argv.toString()
+		let line = argv.join(" ")
 		term.print(line.replace(argv[0], "").trim().replace(/\"/g, "")) 
 	},
-	ls: (argv) => { ls(argv[1]) },
-	cd: (argv) => { cd(argv[1]) },
-	cat: (argv) => { cat(argv[1]) }
+	ls: (argv) => { //ls(argv[1]) 
+		let res = parsePath(argv[1])
+		if (res[0]) {
+			if (res[0].type === 0) {
+				Object.keys(res[0].content).forEach((key) => {
+					term.print(key.substring(1, key.length))
+				})
+			} else {
+				term.print(res[1].substring(res[1].lastIndexOf("/") + 1, res[1].length))
+			}
+		} else {
+			bashError("ls", argv[1], "No such file or directory")
+		}
+	},
+	cd: (argv) => { //cd(argv[1]) 
+		term.print("Not Implemented")
+	},
+	cat: (argv) => { //cat(argv[1]) 
+		term.print("Not Implemented")
+	}
 }
 
 function processInput(input) {
@@ -127,72 +164,6 @@ function processInput(input) {
 		}
 	}
 	term.input(ps1(), processInput)
-}
-
-/**
- * fname: file name to operate on
- * onFile: callback, called when fname is an available file. passed the following arguments:
- * 	- fname: name of file
- * 	- dname: name of directory it exists in (index in dirs)
- * 	- findex: index in file list of that directory
- * onNotFile: callback, called when fname is not an available file (could be a dir). passed the following arguments:
- * 	- fname: name of file
- * 	- dname: name of directory it exists in (index in dirs)
- * 	- findex: index in file list of that directory
- */
-function fileOp(fname, onFile, onNotFile) {
-	fname = cleanPath(fname)
-	for (dname in dirs) {
-		for (findex in dirs[dname]) {
-			if (fname === dname + "/" + dirs[dname][findex]) {
-				onFile(fname, dname, findex)
-				return
-			}
-		}
-	}
-	onNotFile(fname, dname, findex)
-}
-
-function cleanPath(path) {
-	if (![".", "/", "~"].includes(path[0])) path = CWD + "/" + path
-	if (path.endsWith("/")) path = path.substring(0, path.length - 1)
-	path = path.replace("..", CWD.substring(0, CWD.lastIndexOf("/")))
-	path = path.replace(".", CWD)
-	path = path.replace("~", HOME)
-	return path
-}
-
-function bashError(func, arg, error) {
-	term.print("-bash: " + func + ": " + arg + ": " + error)
-}
-
-function ps1() {
-	return "guest@tylergrunenwald.com " + CWD.replace(HOME, "~") + " $ "
-}
-
-function getSubDirs(workingDir) {
-	if (!workingDir) workingDir = CWD
-	let subDirs = []
-	Object.keys(dirs).forEach((key) => { 
-		let regex = new RegExp(workingDir + "/([^/]+)/?.*")
-		let match = regex.exec(key)
-		if (match && !subDirs.includes(match[1])) subDirs.push(match[1]) 
-	})
-	return subDirs
-}
-
-function readLocalFile(fname, onSuccess, onFail) {
-	let PRE = "fs/"
-	let request = new XMLHttpRequest()
-	request.onreadystatechange = function () {
-		if (this.readyState === 4 && (this.status === 200 || this.status === 0)) {
-			onSuccess(this.responseText)
-		} else {
-			onFail()
-		}
-	}
-	request.open("GET", PRE + fname, false)
-	request.send()
 }
 
 // create terminal
